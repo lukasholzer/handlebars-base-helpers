efc340e2a26a1fe88890e858486ad9d724b33504
Lukas Holzer <office@lukasholzer.com>
2016-10-18 00:18:41 +0200

    replace wrong quotations

--- linebreak.js
+++ linebreak.js
@@ -1,6 +1,20 @@
 import handlebars from 'handlebars';
 
 module.exports = function (text) {
-    text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+  text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+  text = text.replace(`„`, `»`);
+  text = text.replace(/["“]/gm, `«`);
+
+
+  // get every uppercase word
+  // const regex = /\b([A-Z]+\s)\b/gm;
+  // let match = regex.exec(text);
+  // while (match !== null) {
+  //   console.log(match);
+  //     console.log("match found at " + match.index);
+  // }
+
+
+
     return new handlebars.SafeString(text);
 };
