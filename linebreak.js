15bf1aaa9cc7dca7dbed26ee4f54fbf28348a584
Lukas Holzer <office@lukasholzer.com>
2016-11-01 18:35:33 +0100

    new quotation

--- linebreak.js
+++ linebreak.js
@@ -3,6 +3,7 @@ import handlebars from 'handlebars';
 module.exports = function (text) {
   text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
   text = text.replace(`„`, `»`);
+  text = text.replace(`„`, `»`);
   text = text.replace(/["“]/gm, `«`);
 
 
