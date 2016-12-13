896140bd9d5fccb2051c5d600f66909aee22a17a
Lukas Holzer <office@lukasholzer.com>
2016-12-13 13:18:52 +0100

    update

--- linebreak.js
+++ linebreak.js
@@ -2,9 +2,9 @@ import handlebars from 'handlebars';
 
 module.exports = function (text) {
   text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
-  text = text.replace(`„`, `»`);
-  text = text.replace(`„`, `»`);
-  text = text.replace(/["“]/gm, `«`);
+  // text = text.replace(`„`, `»`);
+  // text = text.replace(`„`, `»`);
+  // text = text.replace(/["“]/gm, `«`);
 
 
   // get every uppercase word
